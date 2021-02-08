5662db6e540b494f4b7c5f15232aad6f0255e601
James Ma <jama.22@gmail.com>
2021-02-07 20:07:06 -0500

    fixed ci?

--- ci/scripts/create_user.sql
+++ ci/scripts/create_user.sql
@@ -2,3 +2,5 @@ CREATE USER 'test_web'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON snippetbox.* TO 'test_web'@'localhost';
 -- Important: Make sure to swap 'pass' with a password of your own choosing.
 ALTER USER 'test_web'@'localhost' IDENTIFIED BY 'pass'
+
+FLUSH PRIVILEGES;
